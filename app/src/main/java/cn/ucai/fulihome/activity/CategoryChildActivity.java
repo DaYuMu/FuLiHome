b5a8999c3aa41de3081e2d278926f4339ba3c054
大禹来敲门 <2777520544@qq.com>
2016-10-20 20:53:31 +0800

    实现分类小类二级页面的商品价格，上架时间排序后箭头方向的改变

--- app/src/main/java/cn/ucai/fulihome/activity/CategoryChildActivity.java
+++ app/src/main/java/cn/ucai/fulihome/activity/CategoryChildActivity.java
@@ -1,5 +1,6 @@
 package cn.ucai.fulihome.activity;
 
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.support.v4.widget.SwipeRefreshLayout;
 import android.support.v7.widget.GridLayoutManager;
@@ -182,21 +183,30 @@ public class CategoryChildActivity extends BaseActivity {
 
     @OnClick({R.id.button_sort_price, R.id.button_sort_addtime})
     public void onClick(View view) {
+        Drawable right;
         switch (view.getId()) {
             case R.id.button_sort_price:
                 if (PriceAsc) {
                     sortBy = I.SORT_BY_PRICE_ASC;
+                    right = getResources().getDrawable(R.mipmap.arrow_order_up);
                 } else {
                     sortBy = I.SORT_BY_PRICE_DESC;
+                    right = getResources().getDrawable(R.mipmap.arrow_order_down);
                 }
+                right.setBounds(0,0,right.getIntrinsicWidth(),right.getIntrinsicHeight());
+                buttonSortPrice.setCompoundDrawablesRelativeWithIntrinsicBounds(null,null,right,null);
                 PriceAsc = !PriceAsc;
                 break;
             case R.id.button_sort_addtime:
                 if (AddtimeAsc) {
                     sortBy = I.SORT_BY_ADDTIME_ASC;
+                    right = getResources().getDrawable(R.mipmap.arrow_order_up);
                 } else {
                     sortBy = I.SORT_BY_ADDTIME_DESC;
+                    right = getResources().getDrawable(R.mipmap.arrow_order_down);
                 }
+                right.setBounds(0,0,right.getIntrinsicWidth(),right.getIntrinsicHeight());
+                buttonSortAddtime.setCompoundDrawablesRelativeWithIntrinsicBounds(null,null,right,null);
                 AddtimeAsc = !AddtimeAsc;
                 break;
         }
