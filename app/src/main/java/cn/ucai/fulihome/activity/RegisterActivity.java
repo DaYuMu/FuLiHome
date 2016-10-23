fc6d49bbd622b02914050768bcf6704bff4662ac
大禹来敲门 <2777520544@qq.com>
2016-10-23 17:07:56 +0800

    在onRegisterChilk中添加判断注册数据是否合格的判断条件， 在onRegisterChilk中添加register方法。用于判断是否注册成功。

--- app/src/main/java/cn/ucai/fulihome/activity/RegisterActivity.java
+++ app/src/main/java/cn/ucai/fulihome/activity/RegisterActivity.java
@@ -1,6 +1,8 @@
 package cn.ucai.fulihome.activity;
 
+import android.app.ProgressDialog;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.widget.Button;
 import android.widget.TextView;
 
@@ -8,11 +10,18 @@ import butterknife.BindView;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
 import cn.ucai.fulihome.R;
+import cn.ucai.fulihome.bean.Result;
+import cn.ucai.fulihome.net.NetDao;
+import cn.ucai.fulihome.net.OkHttpUtils;
+import cn.ucai.fulihome.utils.CommonUtils;
+import cn.ucai.fulihome.utils.L;
+import cn.ucai.fulihome.utils.MFGT;
 import cn.ucai.fulihome.view.DisplayUtils;
 
 public class RegisterActivity extends BaseActivity {
 
-    String username, usernick, userpassword, userpasswordqr;
+    private static final String TAG = RegisterActivity.class.getSimpleName();
+
     @BindView(R.id.RegisterName)
     TextView RegisterName;
     @BindView(R.id.RegisterNick)
@@ -26,17 +35,24 @@ public class RegisterActivity extends BaseActivity {
     @BindView(R.id.Login)
     TextView Login;
 
+    String username;
+    String usernick;
+    String userpassword;
+
+    RegisterActivity mContext;
+
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         setContentView(R.layout.activity_register);
         ButterKnife.bind(this);
+        mContext = this;
         super.onCreate(savedInstanceState);
     }
 
     @Override
     protected void initView() {
-        DisplayUtils.initBackWithTitle(this,"账号注册");
+        DisplayUtils.initBackWithTitle(this, "账号注册");
     }
 
     @Override
@@ -51,6 +67,66 @@ public class RegisterActivity extends BaseActivity {
 
 
     @OnClick(R.id.RegisterButton)
-    public void onClick() {
+    public void onRegisterChilk() {
+        username = RegisterName.getText().toString().trim();
+        usernick = RegisterNick.getText().toString().trim();
+        userpassword = RegisterPassword.getText().toString().trim();
+        String userpasswordqr = RegisterPasswordqr.getText().toString().trim();
+        if (TextUtils.isEmpty(username)) {
+            CommonUtils.showShortToast(R.string.user_name_connot_be_empty);
+            RegisterName.requestLayout();
+            return;
+        } else if (!username.matches("[a-zA-Z]\\w{5,15}")) {
+            CommonUtils.showShortToast(R.string.illegal_user_name);
+            RegisterName.requestLayout();
+            return;
+        } else if (TextUtils.isEmpty(usernick)) {
+            CommonUtils.showShortToast(R.string.nick_name_connot_be_empty);
+            RegisterNick.requestLayout();
+            return;
+        } else if (TextUtils.isEmpty(userpassword)) {
+            CommonUtils.showShortToast(R.string.password_connot_be_empty);
+            RegisterPassword.requestLayout();
+            return;
+        } else if (TextUtils.isEmpty(userpasswordqr)) {
+            CommonUtils.showShortToast(R.string.confirm_password_connot_be_empty);
+            RegisterPasswordqr.requestLayout();
+            return;
+        } else if (!userpassword.equals(userpasswordqr)) {
+            CommonUtils.showShortToast(R.string.two_input_password);
+            RegisterPasswordqr.requestLayout();
+            return;
+        }
+        register();
+    }
+
+    private void register() {
+        final ProgressDialog pd = new ProgressDialog(mContext);
+        pd.setMessage(getResources().getString(R.string.registering));
+        pd.show();
+        NetDao.register(mContext, username, usernick, userpassword, new OkHttpUtils.OnCompleteListener<Result>() {
+            @Override
+            public void onSuccess(Result result) {
+                pd.dismiss();
+                if (result == null) {
+                    CommonUtils.showLongToast(R.string.register_fail);
+                } else {
+                    if (result.isRetMsg()) {
+                        CommonUtils.showLongToast(R.string.register_success);
+                        MFGT.finish(mContext);
+                    } else {
+                        CommonUtils.showLongToast(R.string.register_fail_exists);
+                        RegisterName.requestLayout();
+                    }
+                }
+            }
+
+            @Override
+            public void onError(String error) {
+                pd.dismiss();
+                CommonUtils.showLongToast(error);
+                L.e(TAG,"Register.onError");
+            }
+        });
     }
 }
