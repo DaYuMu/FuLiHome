5537442c2de022147ec72637f36b9ab788d08bd3
大禹来敲门 <2777520544@qq.com>
2016-10-24 23:00:56 +0800

    新建个人中心的Fragment。在其中实现头像与用户名的显示。

---
+++ app/src/main/java/cn/ucai/fulihome/fragment/PersonalCenterFragment.java
@@ -0,0 +1,69 @@
+package cn.ucai.fulihome.fragment;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+import cn.ucai.fulihome.FuLiHomeApplication;
+import cn.ucai.fulihome.R;
+import cn.ucai.fulihome.activity.MainActivity;
+import cn.ucai.fulihome.bean.User;
+import cn.ucai.fulihome.utils.ImageLoader;
+import cn.ucai.fulihome.utils.L;
+import cn.ucai.fulihome.utils.MFGT;
+
+/**
+ * Created by Administrator on 2016/10/24 0024.
+ */
+public class PersonalCenterFragment extends BaseFragment {
+    private static final String TAG = PersonalCenterFragment.class.getSimpleName();
+    @BindView(R.id.UserAvatar)
+    ImageView UserAvatar;
+    @BindView(R.id.UserName)
+    TextView UserName;
+
+    MainActivity mContext;
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View layout = inflater.inflate(R.layout.fragment_personal_center, null);
+        ButterKnife.bind(this, layout);
+        mContext = (MainActivity) getContext();
+        super.onCreateView(inflater, container, savedInstanceState);
+        return layout;
+    }
+
+    @Override
+    protected void initView() {
+
+    }
+
+    @Override
+    protected void initData() {
+        User user = FuLiHomeApplication.getUser();
+        L.e(TAG+"user"+user);
+        if (user == null) {
+            MFGT.gotoLoginActivity(mContext);
+        } else {
+            ImageLoader.setAvatar(ImageLoader.getAvatar(user),mContext,UserAvatar);
+            UserName.setText(user.getMuserName());
+        }
+    }
+
+    @Override
+    protected void setListener() {
+
+    }
+
+    @OnClick(R.id.Title_Settings)
+    public void onClick() {
+    }
+}
