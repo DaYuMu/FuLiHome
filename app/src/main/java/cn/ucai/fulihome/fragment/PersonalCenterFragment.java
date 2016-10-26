3089b8bb7116c24328e5f3aafb218244ca2673eb
大禹来敲门 <2777520544@qq.com>
2016-10-26 20:37:09 +0800

    实现了收藏商品改变后用户信息的收藏商品数量更新

--- app/src/main/java/cn/ucai/fulihome/fragment/PersonalCenterFragment.java
+++ app/src/main/java/cn/ucai/fulihome/fragment/PersonalCenterFragment.java
@@ -6,6 +6,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import butterknife.BindView;
@@ -14,10 +15,16 @@ import butterknife.OnClick;
 import cn.ucai.fulihome.FuLiHomeApplication;
 import cn.ucai.fulihome.R;
 import cn.ucai.fulihome.activity.MainActivity;
+import cn.ucai.fulihome.bean.MessageBean;
+import cn.ucai.fulihome.bean.Result;
 import cn.ucai.fulihome.bean.User;
+import cn.ucai.fulihome.dao.UserDao;
+import cn.ucai.fulihome.net.NetDao;
+import cn.ucai.fulihome.net.OkHttpUtils;
 import cn.ucai.fulihome.utils.ImageLoader;
 import cn.ucai.fulihome.utils.L;
 import cn.ucai.fulihome.utils.MFGT;
+import cn.ucai.fulihome.utils.ResultUtils;
 
 /**
  * Created by Administrator on 2016/10/24 0024.
@@ -31,6 +38,11 @@ public class PersonalCenterFragment extends BaseFragment {
 
     MainActivity mContext;
     User user;
+    @BindView(R.id.CollectBao)
+    LinearLayout CollectBao;
+    @BindView(R.id.CollectCount)
+    TextView CollectCount;
+
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
@@ -49,11 +61,11 @@ public class PersonalCenterFragment extends BaseFragment {
     @Override
     protected void initData() {
         user = FuLiHomeApplication.getUser();
-        L.e(TAG+"user"+user);
+        L.e(TAG + "user" + user);
         if (user == null) {
             MFGT.gotoLoginActivity(mContext);
         } else {
-            ImageLoader.setAvatar(ImageLoader.getAvatar(user),mContext,UserAvatar);
+            ImageLoader.setAvatar(ImageLoader.getAvatar(user), mContext, UserAvatar);
             UserName.setText(user.getMuserNick());
         }
     }
@@ -67,16 +79,69 @@ public class PersonalCenterFragment extends BaseFragment {
     public void onResume() {
         super.onResume();
         user = FuLiHomeApplication.getUser();
-        L.e(TAG+"user"+user);
+        L.e(TAG + "user" + user);
         if (user != null) {
-            ImageLoader.setAvatar(ImageLoader.getAvatar(user),mContext,UserAvatar);
+            ImageLoader.setAvatar(ImageLoader.getAvatar(user), mContext, UserAvatar);
             UserName.setText(user.getMuserNick());
+            syncUserInfo();
+            syncCollectCount();
         }
     }
 
-    @OnClick({R.id.Title_Settings,R.id.ToSetting})
-    public void onClick() {
-        L.e(TAG+"@OnClick({R.id.Title_Settings,R.id.ToSetting})start");
+    @OnClick({R.id.Title_Settings, R.id.ToSetting})
+    public void toSettings() {
+        L.e(TAG + "@OnClick({R.id.Title_Settings,R.id.ToSetting})start");
         MFGT.gotoSettingActivity(mContext);
     }
+
+    @OnClick(R.id.CollectBao)
+    public void gotoCollectActivity() {
+        MFGT.gotoCollectActivity(mContext);
+    }
+
+    private void syncUserInfo() {
+        NetDao.syncUserInfo(mContext, user.getMuserName(), new OkHttpUtils.OnCompleteListener<String>() {
+            @Override
+            public void onSuccess(String s) {
+                Result result = ResultUtils.getListResultFromJson(s, User.class);
+                if (result != null) {
+                    User u = result.getRetData();
+                    if (!user.equals(u)) {
+                        UserDao dao = new UserDao(mContext);
+                        boolean b = dao.saveUser(u);
+                        if (b) {
+                            FuLiHomeApplication.setUser(u);
+                            user = u;
+                            ImageLoader.setAvatar(ImageLoader.getAvatar(user), mContext, UserAvatar);
+                            UserName.setText(user.getMuserNick());
+                        }
+                    }
+                }
+            }
+
+            @Override
+            public void onError(String error) {
+
+            }
+        });
+    }
+
+    private void syncCollectCount() {
+        NetDao.getCollectCount(mContext, user.getMuserName(), new OkHttpUtils.OnCompleteListener<MessageBean>() {
+            @Override
+            public void onSuccess(MessageBean result) {
+                if (result != null && result.isSuccess()) {
+                    CollectCount.setText(result.getMsg());
+                } else {
+                    CollectCount.setText(String.valueOf(0));
+                }
+            }
+
+            @Override
+            public void onError(String error) {
+                CollectCount.setText(String.valueOf(0));
+            }
+        });
+    }
+
 }
